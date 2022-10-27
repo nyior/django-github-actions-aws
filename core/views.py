0798a2c257611c6e23f5418454d47690cbda7198
briankieffer04 <brian.kieffer@avanade.com>
2022-10-26 20:07:11 -0500

    wowwww

--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
 
 def home(request):
-   text = """<h1>our mind blowing home page goodbye</h1>"""
+   text = """<h1>our mind blowing home page good</h1>"""
    return HttpResponse(text)
